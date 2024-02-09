d86ede0ef5446627ebd0a6e3f029e36d8bcb860d
kimidaisuki22 <40718043+kimidaisuki22@users.noreply.github.com>
2024-02-09 18:04:47 +0800

    change exporter name to . prefixed. add first text to write image.

--- src/exporters/exporter_factory.cpp
+++ src/exporters/exporter_factory.cpp
@@ -3,18 +3,18 @@
 #include "image_process/exporters/exporter_webp.h"
 namespace image_process {
 std::unique_ptr<Exporter> get_exporter_by_name(std::string_view name) {
-  if (name == "jpg") {
+  if (name == ".jpg") {
     return std::make_unique<Exporter_stb_jpg>();
-  } else if (name == "png") {
+  } else if (name == ".png") {
     return std::make_unique<Exporter_stb_png>();
-  } else if (name == "bmp") {
+  } else if (name == ".bmp") {
     return std::make_unique<Exporter_stb_bmp>();
-  } else if (name == "webp") {
+  } else if (name == ".webp") {
     return std::make_unique<Exporter_webp>();
   }
   return nullptr;
 }
 std::vector<std::string_view> get_available_exporter_names() {
-  return {"jpg", "png", "bmp", "webp"};
+  return {".jpg", ".png", ".bmp", ".webp"};
 }
 } // namespace image_process
--- test/test.cpp
+++ test/test.cpp
@@ -1,9 +1,24 @@
+#include "image_process/bitmaps/single_color_bitmap.h"
+#include "image_process/exporters/exporter_factory.h"
+#include <filesystem>
+#include <fstream>
 #include <gtest/gtest.h>
+#include <image_process/exporter.h>
+#include <string>
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+TEST(ImageTest, Write_image) {
+  image_process::Bitmap_single white{};
+  std::string filename = "test_image";
+  for (auto ext : image_process::get_available_exporter_names()) {
+    auto file_path = std::filesystem::path(filename).replace_extension(ext);
+    auto exporter = image_process::get_exporter_by_name(ext);
+    EXPECT_TRUE(exporter) << file_path  <<"\n";
+    auto img = exporter->export_to_buffer(white);
+    EXPECT_TRUE(img);
+    EXPECT_TRUE(!img->empty());
+
+    std::ofstream file{file_path,std::ios::binary};
+
+    file.write(reinterpret_cast<const char*>(img->data()), img->size());
+  }
 }
