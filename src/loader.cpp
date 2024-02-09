2537c31ee8d6758a8faeb82d0f054fbad4650b90
kimidaisuki22 <40718043+kimidaisuki22@users.noreply.github.com>
2024-02-09 13:28:44 +0800

    add loaders into default loader.

--- src/loader.cpp
+++ src/loader.cpp
@@ -1,11 +1,15 @@
 
 #include "image_process/loader.h"
 #include "image_process/loaders/loader_set.h"
+#include "image_process/loaders/loader_stb.h"
+#include "image_process/loaders/loader_webp.h"
 #include <memory>
 namespace image_process {
 
 std::unique_ptr<Loader> get_default_loader() {
     auto loader = std::make_unique<Loader_set>();
+    loader->add_load(std::make_unique<Loader_stb>());
+    loader->add_load(std::make_unique<Loader_webp>());
     return loader;
 }
 }
