da22186d7469c641682c23d6c25eb227d53084ca
kimidaisuki22 <40718043+kimidaisuki22@users.noreply.github.com>
2024-05-01 23:40:17 +0800

    add different size test.

--- benchmark/bm_image_resize.cpp
+++ benchmark/bm_image_resize.cpp
@@ -1,16 +1,12 @@
 #include "image_process/bitmaps/bitmap_flat.h"
-#include <algorithm>
 #include <benchmark/benchmark.h>
-#include <cstdlib>
-#include <cstring>
-#include <map>
-#include <unordered_map>
-#include <vector>
+
 
 static void BM_Bitmap_create(benchmark::State &state) {
+  int size = state.range();
   for (auto _ : state) {
-    image_process::Bitmap_flat bitmap{1024, 1024, 4};
+    image_process::Bitmap_flat bitmap{size, size, 4};
   }
 }
 // Register the function as a benchmark
-BENCHMARK(BM_Bitmap_create);
+BENCHMARK(BM_Bitmap_create)->Range(1, 1024 * 16);
